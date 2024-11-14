392584f7fd87f71d86f0be36c810a1c29589af5e
Hollow1 <jakubvalesilver@gmail.com>
2024-11-14 21:38:58 +0100

    Basic functionality

---
+++ ResolutionChanger/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+class C_Main
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+           string process = "notepad";
+           string command = "nircmd.exe setdisplay 1280 720 32";
+           string original = "nircmd.exe setdisplay 1920 1080 32";
+
+           if (IsProcessRunning(process))
+           {
+               Console.WriteLine($"'{process}' is currently running.");
+               Process.Start("cmd.exe", $"/C {command}");
+           }
+           else
+           {
+               Console.WriteLine($"'{process}' is not running.");
+               Process.Start("cmd.exe", $"/C {original}");
+           }
+           Thread.Sleep(1000); // Check every second
+        }
+    }
+
+    private static bool IsProcessRunning(string processName)
+    {
+        // Get all running processes
+        Process[] processes = Process.GetProcessesByName(processName);
+        return processes.Length > 0; // Return true if any processes are found
+    }
+}
